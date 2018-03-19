922c362fa691d10acc75c0ee9a89d98252e42e04
borfus <mrpeteyb@gmail.com>
2018-03-19 01:32:06 -0500

    Added game project files and build

---
+++ Shootin' Stuff/Assets/Scripts/Resetter.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resetter : MonoBehaviour
+{
+    public Rigidbody2D projectile;
+    public float ResetSpeed = 0.025f;
+    public GameObject canvasObject;
+
+    private float resetSpeedSqr;
+    private SpringJoint2D spring;
+
+    // Use this for initialization
+    void Start()
+    {
+        resetSpeedSqr = ResetSpeed * ResetSpeed;
+        spring = projectile.GetComponent<SpringJoint2D>();
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Reset();
+        }
+
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if(col.tag == "Enemy")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+
+    void Reset()
+    {
+        canvasObject.SetActive(false);
+        Application.LoadLevel(Application.loadedLevel);
+        Time.timeScale = 1;
+    }
+}
---
+++ Shootin' Stuff/Assets/Scripts/WaveMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveMovement : MonoBehaviour
+{
+    public float speed = 2.0f;
+    public float delta = 1.5f; // Amount to move up and down
+    private Vector3 startPos;
+    private float startTime;
+
+    private Vector3 direction = Vector3.left;
+
+    void Start()
+    {
+        startTime = Time.time;
+        startPos = transform.position;
+    }
+
+    void Update()
+    {
+        MoveObject();
+    }
+
+    void MoveObject()
+    {
+        Vector3 v = startPos;
+        v.y += delta * Mathf.Sin(Time.time * speed);
+        v.x += ((startTime - Time.time) * speed);
+        transform.position = v;
+    }
+}
